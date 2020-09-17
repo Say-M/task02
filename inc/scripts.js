14e691d6a01c9562d63a928afebc34148bd05172
Mohammad Sayem <msayem089@gmail.com>
2020-09-17 20:35:42 +0600

    Prject Added

---
+++ inc/scripts.js
@@ -0,0 +1,12 @@
+$(".btn-gender").click(function () {
+    $(".btn-gender").removeClass("btn-primary")
+    $(".btn-gender").addClass("btn-light")
+    $(this).toggleClass("btn-primary");
+    $(this).toggleClass("btn-light");
+})
+$(".btn-marital").click(function () {
+    $(".btn-marital").removeClass("btn-primary")
+    $(".btn-marital").addClass("btn-light")
+    $(this).toggleClass("btn-primary");
+    $(this).toggleClass("btn-light");
+})
